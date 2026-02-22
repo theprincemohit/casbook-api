63e4303c71bf3ce2270d64a119d1019027fb5ac7
Mohit Kumar <theprincemohit@gmail.com>
2026-02-22 17:37:12 +0530

    feat(root): Add root url

--- routers/main.py
+++ routers/main.py
@@ -5,6 +5,10 @@ from routers import business, passbook, transaction, user
 # Get reference from here
 # https://github.com/theprincemohit/full-stack-fastapi-template/blob/master/backend/app/api/main.py
 api_router = APIRouter()
+# Root endpoint
+@api_router.get("/")
+def read_root():
+    return {"message": "Welcome to the Cashbook API"}
 api_router.include_router(business.router)
 api_router.include_router(passbook.router)
 api_router.include_router(transaction.router)
