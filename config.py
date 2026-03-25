9f0ef9c417f0f590aef93b269ab85689fde5074c
Mohit Kumar <theprincemohit@gmail.com>
2026-03-26 00:11:27 +0530

    fix(db-string): db ip

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Database URL
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://postgres:FourMarch@56.228.2.254/:5432/cashbook"
+    "postgresql://postgres:FourMarch@13.49.72.158:5432/cashbook"
 )
 
 # Create engine
